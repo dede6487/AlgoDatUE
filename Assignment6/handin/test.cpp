f420464d261ed15a3dfeac228852207767f6eed9
Felix <dede4861@gmail.com>
2022-11-23 09:50:20 +0100

    makefile

---
+++ Assignment6/handin/test.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+#include "BinSearchTree.h"
+
+using namespace std;
+
+int main() {
+
+	//ERROR: LNK2019 in VS - Verweis auf nicht aufgelöstes externes Symbol
+
+	BinSearchTree<int> a;
+	a.empty();
+	
+	if (a.isempty()) {
+		cout << "a is empty" << endl;
+	}
+	else {
+		cout << "a is not empty" << endl;
+	}
+
+	a.insert(1);
+	a.insert(2);
+	a.insert(3);
+	a.insert(42);
+
+	if (a.member(2)) {
+		cout << "2 is member of a" << endl;
+	}
+	else {
+		cout << "2 is not member of a" << endl;
+	}
+
+	a.del(2);
+
+	if (a.member(2)) {
+		cout << "2 is member of a" << endl;
+	}
+	else {
+		cout << "2 is not member of a" << endl;
+	}
+
+	return 0;
+}
